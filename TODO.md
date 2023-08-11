811173e4ea3feba7542369fa1736710d529b99d8
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-08-11 09:43:45 -0500

    add missing files for aoc
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Roadmap
+
+- Create a project
--- pkg/aoc/aoc.go
+++ pkg/aoc/aoc.go
@@ -18,9 +18,50 @@ func InitializeProject(path string) error {
     if err != nil {
         log.Fatalf("Error creating directory: %s", err)
     }
-    // Create .coe cookie file
-    // Create README.md from a template
-    // Create .gitignore
+    
+    cookieFile := filepath.Join(path, ".aoc")
+    roFile, err := os.OpenFile(cookieFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+    if err != nil {
+        log.Fatalf("Error creating cookie file: %s", err)
+    }
+    
+    defer roFile.Close()
+
+    _, err = roFile.WriteString(`
+# Use the .aoc file to set the environment variables for your project
+session=<insert-advent-of-code-session>
+`)
+    
+    if err != nil {
+        log.Fatalf("Error writing to the aoc file: %s", err)
+    }
+
+    readmeFile := filepath.Join(path, "README.md")
+    roFile, err = os.OpenFile(readmeFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+    if err != nil {
+        log.Fatalf("Error creating README.md file: %s", err)
+    }
+
+    defer roFile.Close()
+    roFile.WriteString(`
+    # Advent of Code
+    `)
+
+
+    gitIgnoreFile := filepath.Join(path, ".gitignore")
+    roFile, err = os.OpenFile(gitIgnoreFile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+    if err != nil {
+        log.Fatalf("Error creating .gitignore file: %s", err)
+    }
+    defer roFile.Close()
+
+    if _, err = roFile.WriteString(`
+# Advent of Code use the .aoc file for storing session information which you should
+# not track in source control.
+.aoc
+`); err != nil {
+		return err
+	}
 
     log.Println("Project initialized")
     return nil
