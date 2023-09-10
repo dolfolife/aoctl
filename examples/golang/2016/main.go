4f52028ce366c0265826de6ef9a38061880b9cdf
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-09-10 10:53:08 -0500

    add run day 2 in main
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- examples/golang/2016/main.go
+++ examples/golang/2016/main.go
@@ -5,9 +5,10 @@ import (
     "fmt"
 
     day_1 "github.com/dolfolife/adventofcode/examples/golang/2016/01"
+    day_2 "github.com/dolfolife/adventofcode/examples/golang/2016/02"
 )
 
-func main() {
+func RunDay1() {
     input, err := os.ReadFile("./01/input.txt")
     if err != nil {
         os.Exit(1)
@@ -18,4 +19,22 @@ func main() {
 
     fmt.Printf("Part 1: %s\n", answers.Part1)
     fmt.Printf("Part 2: %s\n", answers.Part2)
+}
+
+func RunDay2() {
+    input, err := os.ReadFile("./02/input.txt")
+    if err != nil {
+        os.Exit(1)
+    }
+
+    // We need to delete the last rune of the input since it is a newline
+    answers := day_2.Solve(string(input[:len(input)-1]))
+
+    fmt.Printf("Part 1: %s\n", answers.Part1)
+    fmt.Printf("Part 2: %s\n", answers.Part2)
+}
+
+func main() {
+    RunDay1()
+    RunDay2()
 }
