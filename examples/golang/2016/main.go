262b63d4adb255997ff46874e2fe229cad3621fe
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-09-10 12:28:02 -0500

    add day3 to main
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- examples/golang/2016/main.go
+++ examples/golang/2016/main.go
@@ -6,6 +6,7 @@ import (
 
     day_1 "github.com/dolfolife/adventofcode/examples/golang/2016/01"
     day_2 "github.com/dolfolife/adventofcode/examples/golang/2016/02"
+    day_3 "github.com/dolfolife/adventofcode/examples/golang/2016/03"
 )
 
 func RunDay1() {
@@ -34,7 +35,21 @@ func RunDay2() {
     fmt.Printf("Part 2: %s\n", answers.Part2)
 }
 
+func RunDay3() {
+    input, err := os.ReadFile("./03/input.txt")
+    if err != nil {
+        os.Exit(1)
+    }
+
+    // We need to delete the last rune of the input since it is a newline
+    answers := day_3.Solve(string(input[:len(input)-1]))
+
+    fmt.Printf("Part 1: %s\n", answers.Part1)
+    fmt.Printf("Part 2: %s\n", answers.Part2)
+}
+
 func main() {
     RunDay1()
     RunDay2()
+    RunDay3()
 }
